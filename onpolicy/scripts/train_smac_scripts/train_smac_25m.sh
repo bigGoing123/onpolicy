9b434bc82b2af0ff6f88a197642a46b0d1d57241
Mia Ouyang <mia_ouyang@outlook.com>
2025-08-03 19:25:38 +0800

    25-08-03

--- onpolicy/scripts/train_smac_scripts/train_smac_25m.sh
+++ onpolicy/scripts/train_smac_scripts/train_smac_25m.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 env="StarCraft2"
 map="25m"
-algo="rmappo"
+algo="happo"
 exp="check"
-seed_max=1
-
+seed_max=3
+ppo_epochs=15
+ppo_clip=0.05
+steps=2000000
 echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
     CUDA_VISIBLE_DEVICES=0 python ../train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
-    --map_name ${map} --seed ${seed} --n_training_threads 1 --n_rollout_threads 8 --num_mini_batch 1 --episode_length 400 \
-    --num_env_steps 10000000 --ppo_epoch 10 --use_value_active_masks --use_eval --eval_episodes 32
+    --map_name ${map} --seed ${seed} --n_training_threads 8 --n_rollout_threads 16 --num_mini_batch 1 --episode_length 100 \
+    --num_env_steps ${steps} --ppo_epoch 10 --lr 5e-4 --ppo_epoch ${ppo_epochs} --clip_param ${ppo_clip}  \
+    --use_value_active_masks  --eval_episodes 32 --save_interval 100000 --use_bilevel --use_wandb
 done
--- onpolicy/scripts/train_smac_scripts/train_smac_MMM.sh
+++ onpolicy/scripts/train_smac_scripts/train_smac_MMM.sh
@@ -11,5 +11,5 @@ do
     echo "seed is ${seed}:"
     CUDA_VISIBLE_DEVICES=0 python ../train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
     --map_name ${map} --seed ${seed} --n_training_threads 1 --n_rollout_threads 8 --num_mini_batch 1 --episode_length 400 \
-    --num_env_steps 2000000 --ppo_epoch 15 --use_value_active_masks --use_eval --eval_episodes 32 --use_wandb
+    --num_env_steps 2000000 --ppo_epoch 15 --use_value_active_masks  --eval_episodes 32 --use_wandb
 done
