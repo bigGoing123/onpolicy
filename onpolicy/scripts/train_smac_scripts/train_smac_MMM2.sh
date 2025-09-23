9fd3062a04a718fa9139222d93dc435d350746f7
Mia Ouyang <mia_ouyang@outlook.com>
2025-09-23 10:27:27 +0800

    2025-09-23

--- onpolicy/scripts/train_smac_scripts/train_smac_MMM2.sh
+++ onpolicy/scripts/train_smac_scripts/train_smac_MMM2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 env="StarCraft2"
 map="MMM2"
-algo="happo"
+algo="mappo"
 exp="check"
 seed_max=3
 ppo_epochs=10
@@ -11,8 +11,8 @@ echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, max seed is 
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=1 python ../train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
-    --map_name ${map} --seed ${seed} --n_training_threads 8 --n_rollout_threads 16 --num_mini_batch 1 --episode_length 100 \
+    CUDA_VISIBLE_DEVICES=2 python ../train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
+    --map_name ${map} --seed ${seed} --n_training_threads 16 --n_rollout_threads 32 --num_mini_batch 1 --episode_length 100 \
     --num_env_steps ${steps} --ppo_epoch ${ppo_epochs} --clip_param ${ppo_clip} --use_value_active_masks  --eval_episodes 32 \
     --use_wandb --use_bilevel
 done
